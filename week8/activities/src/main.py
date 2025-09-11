7269d6f48bd4953e435c372d906f12107e66e20d
GebwellB <170325747+GebwellB@users.noreply.github.com>
2025-09-11 15:45:09 +0800

    Adding additional files

--- week8/activities/src/main.py
+++ week8/activities/src/main.py
@@ -1,3 +1,7 @@
+import logging
+import random
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -30,6 +34,42 @@ def create_user(user_type, name):
         case _:
             return Viewer(name)
 
+class UploadNotifier:
+    def __init__(self):
+        self.subscribers = []
+
+    def subscribe(self, fn):
+        self.subscribers.append(fn)
+
+    def notify(self, document_name):
+        for fn in self.subscribers:
+                fn(document_name)
+
+def alert_admin(doc):
+    logging.info(f"Admin notified: {doc} uploaded")
+
+def log_upload(doc):
+    logging.info(f"Log notified: {doc} uploaded by")
+
+
+notifier = UploadNotifier()
+notifier.subscribe(alert_admin)
+notifier.subscribe(log_upload)
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def action_logger(func):
+    def wrapper(*args, **kwargs):
+        user = args[0]
+        logging.info(f"[ACTION] {user.name} ({user.role()}) has run {func.__name__}")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+@action_logger
+def upload_document(user, document):
+    print(f'{user.name} ({user.role()}), uploaded {document}')
+    notifier.notify(document)
 
 if __name__ == "__main__":
     users = [
@@ -42,6 +82,8 @@ if __name__ == "__main__":
         create_user('admin', 'Dylan'),
     ]
 
-    for user in users:
-        print(user.name)
-        print(user.role)
+    # for user in users:
+    #    print(user.name)
+    #    print(user.role)
+    for i in range(10):
+        upload_document(users[random.randint(0,6)], 'project_plan_v1.pdf')
